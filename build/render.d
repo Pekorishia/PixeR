a311cdf34de332951c453dec4a016a59ff871d37
Patricia <patipc@hotmail.com>
2017-08-16 16:50:24 -0300

    sphere normal-based color picking added

---
+++ build/render.d
@@ -0,0 +1,10 @@
+build/render.o: src/render.cpp src/../utility/vec3.h \
+ src/../utility/vec3.inl src/../utility/vec3.h src/../utility/ray.h
+
+src/../utility/vec3.h:
+
+src/../utility/vec3.inl:
+
+src/../utility/vec3.h:
+
+src/../utility/ray.h:
--- src/render.cpp
+++ src/render.cpp
@@ -1,202 +1,257 @@
 #include <iostream>
+#include <sstream>
+#include <string> 
 #include <fstream>
-#include <limits>
-#include <string>
-#include <math.h> 
-#include <cmath>     
-
+#include <vector>
 #include "../utility/vec3.h"
 #include "../utility/ray.h"
 
 using namespace utility;
 
-/*
- *  In the near future we'll want to refactor our project to become
- *  something like the code below.
- */
-#ifdef TRADITIONAL_RT
-Image Raytrace (Camera cam, Scene scene, int width, int height)
+using namespace std;
+
+//==============================[ GLOBALS ]====================================//
+
+float up_left[3];
+float up_right[3];
+float down_right[3];
+float down_left[3];
+
+//==============================[ FILE HANDLING ]==============================//
+
+const vector<string> split(const string& s, const char& c)
 {
-	Image image = new Image (width, height) ;
-	for (int i = 0 ; i < height ; i++)
-		for (int j = 0 ; j < width ; j++) {
-			Ray ray = RayThruPixel (cam, i, j) ;
-			Intersection hit = Intersect (ray, scene) ;
-			image[i][j] = FindColor (hit) ;
-			}
-	return image ;
-}
-#endif
-
-/*
- * Returns de Z component, if the ray hits the sphere, or infinity otherwise
- * 
- * TODO: only works with a 2D sphere
- */
-float hit_sphere ( const Ray & r_, const point3 & c_, float radius_ ){
-
-	// First, manipulate the 1), 2) and 3) line to create the 4) equation
-	// 1) (point - center) * (point - center) - radius² = 0
-	// 2) point == ray
-	// 3) ray(t) = origin + direction*t
-	// 4) d*d*t² + 2(origin - center)*d*t + (origin - center)*(origin - center) - radius² = 0
-
-
-	// Second, calculate the a, b and c from the equation 4) (a*t² + b*t + c = 0) 
-	auto oc = r_.get_origin() - c_; // (origin - center)
-
-	auto a = utility::dot( r_.get_direction(), r_.get_direction() );
-
-	auto b = 2 * utility::dot( oc, r_.get_direction() );
-
-	auto c = utility::dot(oc, oc) - (radius_ * radius_);
-
-	// Third, calculate the delta (b² - 4ac) is >= 0
-	auto delta = b * b - 4 * a * c;
-
-	// Last, return the Z component
-	if (delta >= 0){
-		return std::abs (c_.z());
-	}
-
-	return std::numeric_limits<float>::infinity();
-}
-
-//float euclidian_distance (const Ray & r_, const point3 & c_){
-//    float distance = sqrt(pow (r_.x() - c_.x(), 2) + pow (r_.y() - c_.y(),2));
-//    return distance;
-//}
-
-//rgb radial_gradient (const Ray & r_, const point3 & c_, float radius_){
-//	float distance = euclidian_distance(r_, c_);
-//	int val = 255.0/radius_;
-
-    //return the gradient
-//    return rgb (255 - (((int)val * (int)distance) % 255), 0, 0);
-//}
-
-/*
- * Find the resulting color of the horizontal interpolation
- * 
- * TODO: only work with the range [-2;2]
- */
-rgb horizontal_interpolation ( const Ray & r_, const rgb & left, const rgb & right){
-	// 1) we make the ray a vector in the same direction.
-    auto ray = r_.get_direction();
-
-    // 2) we take only the horizontal component
-    auto ray_x = ray.x(); // this component might assume values ranging from -2 to 2
-
-    // 3) normalize the ray's horizontal component to the range [0;1]
-    auto t= 0.5+( ray_x*0.25 );
-
-    // 4) use linear interpolation (lerp) between the colors that compose the background
-    rgb result = left*(1 - t) + right*(t);
-
-    return result;
+    string buff{""};
+    vector<string> v;
+    
+    for(auto n:s)
+    {
+        if(n != c) buff+=n; else
+        if(n == c && buff != "") { v.push_back(buff); buff = ""; }
+    }
+    if(buff != "") v.push_back(buff);
+    
+    return v;
 }
 
 /*
- * Find the resulting color of the vertical interpolation
- *
- * TODO: only work with the range [-1;1]
+ *  Opens and read the scene text explanation file
  */
-rgb vertical_interpolation ( const Ray & r_, const rgb & bottom, const rgb & top){
-	// 1) we make the ray a vector in the same direction.
-    auto ray = r_.get_direction();
+void file_handler (std::string & cena, std::string & line, std::string & name, int & width, int & height)
+{
+    std::ifstream arquivo_cena;
 
-    // 2) we take only the vertical component, since the lerp has to interpolate colors verticaly
-    auto ray_y = ray.y(); // this component might assume values ranging from -1 to 1
+    arquivo_cena.open (cena, std::ofstream::in);
 
-    // 3) normalize the ray's vertical component to the range [0;1]
-    auto t= 0.5+( ray_y*0.5 );
+    while(std::getline (arquivo_cena,line)){
+        vector<string> v{split(line, '=')};
+        for(unsigned int i=0; i<v.size(); i++) {
+            if(v[i] == "NAME")
+                name = v[i+1];
+            if(v[i] == "WIDTH")
+                width = atoi(v[i+1].c_str());
+            if(v[i] == "HEIGHT")
+                height = atoi(v[i+1].c_str());
+            if (v[i] == "UPPER_LEFT")
+            {
+                vector<string> w{split(v[i+1], ' ')};
+                for (int j = 0; j < 3; ++j)
+                {
+                    up_left[j] = atof(w[j].c_str())/255;
+                }                
+            }
+            if (v[i] == "LOWER_LEFT")
+            {
+                vector<string> w{split(v[i+1], ' ')};
+                for (int j = 0; j < 3; ++j)
+                {
+                    down_left[j] = atof(w[j].c_str())/255;
+                }
+            }
+            if (v[i] == "UPPER_RIGHT")
+            {
+                vector<string> w{split(v[i+1], ' ')};
+                for (int j = 0; j < 3; ++j)
+                {
+                    up_right[j] = atof(w[j].c_str())/255;
+                }
+            }
+            if (v[i] == "LOWER_RIGHT")
+            {
+                vector<string> w{split(v[i+1], ' ')};
+                for (int j = 0; j < 3; ++j)
+                {
+                    down_right[j] = atof(w[j].c_str())/255;
+                }
+            }
+        }
+    }
 
-    // 4) use linear interpolation (lerp) between the colors that compose the background
-    rgb result = bottom*(1 - t) + top*(t);
+    arquivo_cena.close();
+}
+
+//==============================[ COLLISIONS ]=================================//
+
+/*  
+ * Returns de Z component, if the ray hits the sphere, or infinity otherwise  
+ *  
+ * TODO: only works with a 2D sphere  
+ */ 
+float hit_sphere ( const Ray & r_, const point3 & c_, float radius_ )
+{  
+    // First, manipulate the 1), 2) and 3) line to create the 4) equation   
+    // 1) (point - center) * (point - center) - radius² = 0     
+    // 2) point == ray  // 3) ray(t) = origin + direction*t     
+    // 4) d*d*t² + 2(origin - center)*d*t + (origin - center)*(origin - center) - radius² = 0   
+    
+    // Second, calculate the a, b and c from the equation 4) (a*t² + b*t + c = 0)   
+    auto oc = r_.get_origin() - c_; // (origin - center)    
+    
+    auto a = utility::dot( r_.get_direction(), r_.get_direction() );   // d*d  
+    auto b = 2 * utility::dot( oc, r_.get_direction() );               // 2(origin - center)*d
+    auto c = utility::dot(oc, oc) - (radius_ * radius_);               // (origin - center)*(origin - center) - radius²
+
+    // Third, calculate the delta (b² - 4ac)    
+    auto delta = b * b - 4 * a * c;     
+
+    // Last, return the t component if the ray hit the sphere 
+    if (delta >= 0){
+        auto t = (-b - sqrt(delta))/(2*a);
+        return t;   
+    }
+
+    // or return infinity otherwise
+    return std::numeric_limits<float>::infinity(); 
+} 
+
+//==============================[ INTERPOLATIONS ]=============================//  
+
+
+/*  
+ * Find the resulting color of the horizontal interpolation  
+ *  
+ * TODO: only work with the range [-2;2]  
+ */ 
+rgb horizontal_interpolation ( const Ray & r_, const rgb & left, const rgb & right)
+ {     
+    // 1) we make the ray a vector in the same direction.     
+    auto ray = r_.get_direction();     
+
+    // 2) we take only the horizontal component     
+    auto ray_x = ray.x(); // this component might assume values ranging from -2 to 2     
+
+    // 3) normalize the ray's horizontal component to the range [0;1]     
+    auto t= 0.5+( ray_x*0.25 );     
+
+    // 4) use linear interpolation (lerp) between the colors that compose the background     
+    rgb result = left*(1 - t) + right*(t);     
+
+    return result; 
+} 
+
+/*  
+ * Find the resulting color of the vertical interpolation  
+ *  
+ * TODO: only work with the range [-1;1]  
+ */ 
+rgb vertical_interpolation ( const Ray & r_, const rgb & bottom, const rgb & top)
+{    
+    // 1) we make the ray a vector in the same direction.     
+    auto ray = r_.get_direction();     
+
+    // 2) we take only the vertical component, since the lerp has to interpolate colors verticaly     
+    auto ray_y = ray.y(); // this component might assume values ranging from -1 to 1     
+
+    // 3) normalize the ray's vertical component to the range [0;1]     
+    auto t= 0.5+( ray_y*0.5 );     
+
+    // 4) use linear interpolation (lerp) between the colors that compose the background     
+    rgb result = bottom*(1 - t) + top*(t);     
+    return result; 
+}
+
+//==============================[ COLOR PICKING ]==============================//
+
+/*  
+ * Returns de color of the specific pixel that is hit by the ray  
+ *  
+ * TODO: only works with a view plane with aspect ratio 2:1
+ */ 
+rgb color( const Ray & r_ )
+{
+    rgb UPPER_LEFT (up_left[0], up_left[1], up_left[2]);
+    rgb LOWER_LEFT (down_left[0], down_left[1], down_left[2]);
+    rgb UPPER_RIGHT (up_right[0], up_right[1], up_right[2]);
+    rgb LOWER_RIGHT (down_right[0], down_right[1], down_right[2]);
+
+    point3 c (0,0,-1);
+
+    auto sphere_t = hit_sphere(r_, c, 0.5);
+
+    if(sphere_t != std::numeric_limits<float>::infinity()){
+
+        point3 p = r_.point_at(sphere_t);
+
+        vec3 normal = p - c;
+
+        auto unit_normal = utility::unit_vector(normal);
+        auto color_sphere = 0.5* (unit_normal + vec3(1,1,1));
+        
+        return color_sphere;
+    }
+
+
+    auto unit_ray = (r_.get_direction());
+
+    auto unit_ray_y = unit_ray.y();
+    auto unit_ray_x = unit_ray.x();
+
+    auto t = (0.5*unit_ray_y)+0.5;
+    auto u = (0.25*unit_ray_x)+0.5;
+
+    rgb result = (LOWER_LEFT*(1-t)*(1-u) +
+                    UPPER_LEFT*t*(1-u) +
+                    LOWER_RIGHT*(1-t)*(u) +
+                    UPPER_RIGHT*(t)*(u)
+                    );
 
     return result; 
 }
 
+//==============================[ MAIN ]=====================================//
 
-rgb color( const Ray & r_ ){
-	rgb top_left     (0, 1, 0); // green
-	rgb top_right    (1, 1, 0); // yellow
-	rgb bottom_left  (0, 0, 0); // black
-	rgb bottom_right (1, 0, 0); // red
+/*  
+ * Responsible for the generation of the final image  
+ *  
+ * TODO: the camera is static
+ * TODO: only make ASCII files
+ */ 
+int main( int argc, const char * argv[] )
+{
+    std::string cena, line, name="";
+    int width=0, height=0;
 
+    if(argc >1)
+        cena = std::string(argv[1]);
     
-    //float hit1 = hit_sphere (r_, point3(0,0,-1), 0.2);
-	
-    //if ( hit1 < std::numeric_limits<float>::infinity()){
-    //	return radial_gradient (r_, point3(0,0,-1), 0.2);
-    //}
+    file_handler (cena, line, name, width, height);
+
+    int n_cols{ width };
+    int n_rows{ height };
+
+    std::ofstream arq;
     
+    std::string path = "../assets/";
+    path = path + name;
 
-    rgb top = horizontal_interpolation(r_, top_left, top_right);
-    rgb bottom = horizontal_interpolation(r_, bottom_left, bottom_right);
+    arq.open (path.c_str(), std::ofstream::trunc);
 
-    rgb result = vertical_interpolation(r_, bottom, top);
-    
-    return result;
-}
-
-/*
- * Opens the scene file and get all values
- */
-//void file_handler( std::string & file_name, 
-//				   std::string & img_name, 
-//				   std::string & type,
-//				   std::string & codification,
-//				   int & width,
-//				   int & height,
-//				   rgb & top_left,
-//				   rgb & bottom_left,
-//				   rgb & top_right,
-//				   rgb & bottom_right){
-//
-//	std::string line;
-//  	std::ifstream ifs(file_name);
-//
-//  	getline (ifs,line);
-//
-//  	std::cout << line << ".\n";
-//
-//    // Remove comments
-//    if (line.find("#") < line.length()) {
-//        line = line.replace(line.find("#"), line.length() - 1, "");
-//    }
-//
-//    std::cout << line << std::endl;
-//    std::string n("    UPPER_LEFT: ");
-//    line = line.replace(0, n.length(), "");
-//    std::cout << line << std::endl;
-//    n = line.substr(0, line.find(" "));
-//    float r = atof(n.c_str());
-//    line = line.replace(1, line.find(" "), "");
-//    std::cout << line << std::endl;
-//    n = line.substr(0, line.find(" "));
-//    std::cout << n << std::endl;
-//    float g = atof(n.c_str());
-//    n = line.substr(line.find(" "));
-//    std::cout << n << std::endl;
-//    float b = atof(n.c_str());
-//    std::cout << r << " - " << g << " - " << b << std::endl;
-//}
-
-int main (int argc, char **argv){
-	
-	//file_handler(argv[1]);	
-
-    int n_cols{ 200 };
-    int n_rows{ 100 };
-
-    std::cout << "P3\n"
-              << n_cols << " " << n_rows << "\n"
-              << "255\n";
+    std::stringstream ss;
+    ss << "P3\n" << n_cols << " " << n_rows << "\n255\n";
 
     //=== Defining our 'camera'
-    point3 lower_left_corner( -2, -1, -1 ); // lower left corner of the view plane.
-    vec3 horizontal( 4, 0, 0 ); // Horizontal dimension of the view plane.
+    point3 lower_left_corner(-2, -1, -1); // lower left corner of the view plane.
+    vec3 horizontal(4, 0, 0); // Horizontal dimension of the view plane.
     vec3 vertical(0, 2, 0); // Vertical dimension of the view plane.
     point3 origin(0, 0, 0); // the camera's origin.
 
@@ -229,9 +284,12 @@ int main (int argc, char **argv){
             int ig = int( 255.99f * c[rgb::G] );
             int ib = int( 255.99f * c[rgb::B] );
 
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            ss << ir << " " << ig << " " << ib << "\n";
+                     
         }
     }
+    arq << ss.str();
+    arq.close();
 
     return 0;
 }
