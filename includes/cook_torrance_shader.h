fe0a5b4d78548e87fd3c973dd9e572833640a547
Debora Costa <debora@debora.debora>
2018-05-27 15:10:01 -0300

    finalizado o cook-torrance com a distribuição de blinnphong e beckmann

--- includes/cook_torrance_shader.h
+++ includes/cook_torrance_shader.h
@@ -24,24 +24,26 @@ class CookTorranceShader : public Shader
 
 float CookTorranceShader::schlick(vec3 l, vec3 h, float f0) const {
     float cosine = std::max(0.f, dot(l, h));
-    float r = (1.f - f0) / (1.f + f0);
-    r = r * r;
-    //return (r + (1.f - r) * std::pow((1.f - cosine), 5.f));
     return f0 + std::pow(1-cosine, 5)*(1-f0);
 }
 
 float CookTorranceShader::d(vec3 h, vec3 n, float m) const {
-    float cosine = std::max(0.f, dot(n, h)) / (n.length() * h.length() ); //n]ao sei se esse calculo ta certo
-	float NdotH =  dot(n, h);
-    float e = -1* ( (1 - cosine*cosine) / (m*m * cosine*cosine) );
-    float x = PI * m*m * cosine*cosine*cosine*cosine;
 
+    float cosine = dot(n, h) / (n.length() * h.length() );
+	float NdotH =  dot(n, h);
+
+	float a = (2/(m*m)) -2;
+	return ((a + 2) / 2 * PI) * pow(NdotH, a); //blinnphong
+
+    float e = ( (NdotH*NdotH -1) / (m*m * NdotH*NdotH) );
+    float x = 1.f/(PI * m*m * NdotH*NdotH*NdotH*NdotH);
+    return std::exp(e)*x; //beckmann
 
     float m_2 = m*m;
     float r1 = 1.0/ (4.0 * m_2* std::pow(NdotH, 4));
     float r2 = (NdotH * NdotH -1.0)/ (m_2 * NdotH * NdotH);
-    return std::pow(1.0/(std::pow(m,2.0)*std::pow(NdotH,4.0)), ( std::pow(NdotH,2.0)-1.0)/( std::pow(m,2.0)*pow(NdotH,2.0)));
-    return exp(e)/x;
+    //return std::pow(1.0/(std::pow(m,2.0)*std::pow(NdotH,4.0)), ( std::pow(NdotH,2.0)-1.0)/( std::pow(m,2.0)*pow(NdotH,2.0)));
+    
 }
 
 float CookTorranceShader::g(vec3 l, vec3 v, vec3 h, vec3 n) const {
@@ -65,13 +67,13 @@ rgb CookTorranceShader::color( const Ray & r_, float t_min, float t_max, int dep
     { 
         rgb cor(0,0,0);
 
-        vec3 v = unit_vector(r_.get_direction());
+        vec3 v = unit_vector(-r_.get_direction());
 
-        vec3 n = unit_vector(ht.normal);
+        vec3 n = (ht.normal);
 
         auto ambient = rgb(0.1,0,0.1)  * world->ambientLight;
 
-        cor += ambient;
+        //cor += ambient;
 
         for( int i = 0; i < Shader::world->lum_size; i++){
 
@@ -98,9 +100,11 @@ rgb CookTorranceShader::color( const Ray & r_, float t_min, float t_max, int dep
         		float fr = ( schlick(l, h, 0.5)* d(h, n, ht.mat->m)* g(l, v, h, n) ) / 
         					(4 * (n_l) * (n_v));
 
-        		//std::cout << " F:" << schlick(l, h, ht.mat->m) << " D:"<< d(h, n, ht.mat->m) << " G:" <<g(l, v, h, n);
+        		//std::cout <<" fr: "<< fr << " F:" << schlick(l, h, ht.mat->m) << " D:"<< d(h, n, ht.mat->m) << " G:" <<g(l, v, h, n);
 
-        		cor += n_l * ( world->lum[i]->getIntensity() * fr + difuse);//(difuse + fr * (vec3(1,1,1) - ht.mat->albedo->value(0,0, vec3(0,0,0))));
+
+        		cor = n_l * (( world->lum[i]->getIntensity() * fr ) + difuse);//(difuse + fr * (vec3(1,1,1) - ht.mat->albedo->value(0,0, vec3(0,0,0))));
+        		//cor += difuse * world->lum[i]->getIntensity() * fr;
 
             }
 
